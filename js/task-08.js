fe7e6695d57c4f302a54cd190266fcf3259791a7
Volodymyr1973 <vlrohmanov@gmail.com>
2022-09-17 21:13:30 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formEl = document.querySelector(`.login-form`);
 
+let loginEl = {};
+
+const onSendLogin = (event) => {
+  event.preventDefault();
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+  if (email === "" || password === "") {
+    alert(`Заполните все поля форм!!!!!!!`);
+  } else loginEl.email = `${email}`;
+  loginEl.password = `${password}`;
+  console.log(loginEl);
+};
+
+formEl.addEventListener("submit", onSendLogin);
