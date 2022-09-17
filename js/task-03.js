84cd48b94536193c4d9b8fc560e5d5392942144c
Volodymyr1973 <vlrohmanov@gmail.com>
2022-09-17 12:51:37 +0300

    task3

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,48 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const galleryEl = document.querySelector(`.gallery`);
+
+// const allItemImgEl = images.map((image) => {
+//   const itemImgEl = document.createElement(`li`);
+//   itemImgEl.classList.add(`gallery__item`);
+
+//   const imageCardEl = document.createElement(`img`);
+//   imageCardEl.setAttribute(`url`, `${image.url}`);
+//   imageCardEl.setAttribute(`alt`, `${image.alt}`);
+
+//   itemImgEl.append(imageCardEl);
+
+//   return itemImgEl;
+// });
+
+// console.log(allItemImgEl);
+
+// const galleryCard = allItemImgEl.join("");
+
+// galleryEl.insertAdjacentHTML(`afterbegin`, "galleryCard");
+
+const allItemImgEl = images
+  .map(
+    (image) =>
+      `<li class="item"><img url="${image.url}" alt="${image.alt}" width="480"/></li>`
+  )
+  .join(" ");
+
+console.log(allItemImgEl);
+
+galleryEl.insertAdjacentHTML("afterbegin", allItemImgEl);
+
+console.log(galleryEl);
