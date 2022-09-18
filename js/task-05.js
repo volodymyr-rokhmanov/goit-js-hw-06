f9e9c3c9b19e4717fc79fc2b447d0407c4e1df8a
Volodymyr1973 <vlrohmanov@gmail.com>
2022-09-18 13:41:56 +0300

    hw correction

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,8 @@
 const inputEl = document.querySelector(`#name-input`);
-console.log(inputEl);
 
 const outputEl = document.querySelector(`#name-output`);
-console.log(outputEl);
 
 const onClick = (event) => {
-  console.log(event);
   if (event.target.value === "") {
     outputEl.textContent = "Anonymous";
   } else outputEl.textContent = event.target.value;
